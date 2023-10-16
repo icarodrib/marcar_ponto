d19bd1751dad828ebf690aae1e3e2ef2dc182a9b
icarodrib <icarodrib@gmail.com>
2023-10-16 09:59:22 -0300

    Ultima modificação do codigo fonte

--- App.py
+++ App.py
@@ -1,27 +1,72 @@
+import tkinter as tk
 import psycopg2
 
-# Parâmetros para a conexão com o banco de dados
-host = "127.0.0.1"
-database = "e-enterprise"
-user = "postgres"
-password = "123"
+# Conectar-se ao banco de dados PostgreSQL
+conn = psycopg2.connect(
+    host = "127.0.0.1",
+    database = "e-enterprise",
+    user = "postgres",
+    password = "123"
+)
+cursor = conn.cursor()
 
-# Estabelecer a conexão
-conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+# Função para calcular horas trabalhadas e extras
+def calcular_horas():
+    id_funcionario = int(id_funcionario_entry.get())
+    data = data_entry.get()
+    hora_base = int(hora_base_entry.get())
+    
+    horas_trabalhadas = f"{int(horas_trabalhadas_entry1.get())}:{int(horas_trabalhadas_entry2.get())}:{int(horas_trabalhadas_entry3.get())}"
 
-# Criar um cursor a partir da conexão para executar comandos SQL
-cur = conn.cursor()
+    horas_extras = int(horas_trabalhadas_entry1.get()) - hora_base
 
-# Executar uma consulta SQL
-cur.execute("SELECT * FROM FUNCIONARIO")
+    # Inserir os dados no banco de dados
+    cursor.execute("INSERT INTO horas_trabalhadas (id_funcionario, data, hora_base, horas_trabalhadas, horas_extras) VALUES (%s, %s, %s, %s, %s)",
+                   (id_funcionario, data, hora_base, horas_trabalhadas, horas_extras))
+    conn.commit()
 
-# Recuperar os resultados
-resultados = cur.fetchall()
+    resultado_label.config(text=f"Horas trabalhadas: {horas_trabalhadas} horas\nHoras extras: {horas_extras} horas")
 
-# Imprimir os resultados
-for linha in resultados:
-    print(linha)
+# Criar a janela principal
+root = tk.Tk()
+root.title("Contador de Banco de Horas")
 
-# Fechar o cursor e a conexão
-cur.close()
+# Elementos da interface
+id_funcionario_label = tk.Label(root, text="ID do Funcionário:")
+id_funcionario_entry = tk.Entry(root)
+data_label = tk.Label(root, text="Data (AAAA-MM-DD):")
+data_entry = tk.Entry(root)
+hora_base_label = tk.Label(root, text="Hora Base:")
+hora_base_entry = tk.Entry(root)
+horas_trabalhadas_label = tk.Label(root, text="Horas Trabalhadas:")
+horas_trabalhadas_label1 = tk.Label(root, text="Horas")
+horas_trabalhadas_entry1 = tk.Entry(root)
+horas_trabalhadas_label2 = tk.Label(root, text="Minutos")
+horas_trabalhadas_entry2 = tk.Entry(root)
+horas_trabalhadas_label3 = tk.Label(root, text="Segundos")
+horas_trabalhadas_entry3 = tk.Entry(root)
+calcular_button = tk.Button(root, text="Calcular Horas", command=calcular_horas)
+resultado_label = tk.Label(root, text="")
+
+# Posicionar elementos na interface
+id_funcionario_label.pack()
+id_funcionario_entry.pack()
+data_label.pack()
+data_entry.pack()
+hora_base_label.pack()
+hora_base_entry.pack()
+horas_trabalhadas_label.pack()
+horas_trabalhadas_label1.pack()
+horas_trabalhadas_entry1.pack()
+horas_trabalhadas_label2.pack()
+horas_trabalhadas_entry2.pack()
+horas_trabalhadas_label3.pack()
+horas_trabalhadas_entry3.pack()
+calcular_button.pack()
+resultado_label.pack()
+
+# Iniciar a aplicação
+root.mainloop()
+
+# Fechar a conexão com o banco de dados quando a janela for fechada
 conn.close()
